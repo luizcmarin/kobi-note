b673886f902e35a2bc5de3de17fd2776f46171db
Luiz Marin <67489841+luizcmarin@users.noreply.github.com>
2026-09-01 16:29:53 -0300

    Publica o Kobi Note — aplicativo e acervo (999 registros)

--- assets/qr-code-CZUadAne.js
+++ assets/qr-code-CZUadAne.js
@@ -1,4 +1,4 @@
-import{C as e,D as t,E as n,S as r,T as i,_ as a,g as o,h as s,m as c,p as l,t as u,v as d,w as f,y as p}from"./index-CkFHdhih.js";var m=p`
+import{C as e,D as t,E as n,S as r,T as i,_ as a,g as o,h as s,m as c,p as l,t as u,v as d,w as f,y as p}from"./index-DL0gZq2M.js";var m=p`
   :host {
     display: inline-block;
   }
